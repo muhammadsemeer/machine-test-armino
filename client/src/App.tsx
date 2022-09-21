3ff48c7d66fde29c5a545e8048625091f3f16a5c
muhammadsemeer <muhammadsemeerali@gmail.com>
2022-09-21 15:39:37 +0530

    added userId

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom";
+import { v4 } from "uuid";
 import NavBar from "./components/NavBar";
 import Index from "./pages";
 
-const App = () => (
-    <>
-        <NavBar />
-        <Routes>
+const App = () => {
+    const userId = localStorage.getItem("userId");
+
+    if (!userId) {
+        localStorage.setItem("userId", v4());
+    }
+
+    return (
+        <>
+            <NavBar />
+            <Routes>
                 <Route path="/" element={<Index />} />
             </Routes>
-    </>
-);
+        </>
+    );
+};
 
 export default App;
